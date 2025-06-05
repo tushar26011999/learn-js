4b4200e408ac76e70d69c1c79a032b401daa1488
tushar parashar <tushar26011999@gmail.com>
2025-06-05 21:17:03 +0530

    siblings target

---
+++ 04 Siblings/script.js
@@ -0,0 +1,11 @@
+// let firstLi = document.querySelector("li");
+// let ul = document.querySelector("ul");
+
+// console.log(ul.children)
+// console.log(ul.children[0].innerText = "hello")
+// console.log(firstLi.parentElement)
+
+// console.log(firstLi.nextElementSibling.textContent);
+
+// let fourthLi = document.querySelector(".fourth");
+// console.log(fourthLi.previousElementSibling.textContent);
