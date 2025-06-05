3980ebc5d5a78f0352f70db411f91638004008a4
tushar parashar <tushar26011999@gmail.com>
2025-06-05 21:18:12 +0530

    Elements append remove

---
+++ 06 Elements append, remove etc/script.js
@@ -0,0 +1,29 @@
+// const h1 = document.createElement('h1');
+// const body = document.body;
+// h1.textContent = "hello world";
+// h1.classList.add('heading');
+// body.appendChild(h1);
+// const li1 = document.createElement('li');
+// li1.textContent = "item 1";
+// h1.insertBefore(li1, h1.firstChild);
+
+// const firstP = document.querySelector("p");
+// const i = document.createElement("i");
+// i.textContent = "I'm Italics";
+// i.style.backgroundColor = "red";
+// i.style.padding = "5rem";
+// firstP.insertAdjacentElement("beforebegin", i);
+
+// const section = document.querySelector("section");
+// const div = document.createElement("div");
+// const div2 = document.createElement("div");
+// const div3 = document.createElement("div");
+// const div4 = document.createElement("div");
+// div.textContent = "I'm a div";
+// div2.textContent = "I'm a div";
+// div3.textContent = "I'm a div";
+// section.append(div, div2, div3, div4);
+// section.prepend(div, div2, div3, div4);
+
+const newList = document.querySelectorAll("ul");
+newList[0].remove()
