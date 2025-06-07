99a17cea2ed9ca300825451ce3abacfddd118993
tushar parashar <tushar26011999@gmail.com>
2025-06-07 22:16:58 +0530

    project till events

---
+++ 10 Projects 2/script.js
@@ -0,0 +1,14 @@
+// console.log(firstName);
+
+// var firstName = "John";
+// let lastName = "Doe";
+// const age = 30;
+
+// alert("Hello World!");
+// confirm("Are you sure?");
+// prompt("What is your name?");
+
+const last4Digits = "1234";
+// console.log(last4Digits.padStart(12, "*")); // ****1234
+console.log(last4Digits.split(""));
+ 
