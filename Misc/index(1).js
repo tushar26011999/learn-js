a24a4ba1b2c6b8cf3b1ba163a9ef2c6d3d502c2a
tushar parashar <tushar26011999@gmail.com>
2025-06-22 23:14:31 +0530

    excercises and misc problems

---
+++ Misc/index(1).js
@@ -0,0 +1,109 @@
+// console.log("hello");
+
+// let design1 = '24';
+// let design2 = 23;
+// console.log(design1 > design2);
+
+// const userAge = 18;
+
+// const isCollegeStudent = userAge >= 18 && userAge <= 22;
+// console.log(isCollegeStudent);
+
+// const dayNum = 2;
+
+// switch (dayNum) {
+//   case 0:
+//     console.log("Sunday");
+//     // break;
+//   case 1:
+//     console.log("Monday");
+//     // break;
+//   case 2:
+//     console.log("Tuesday");
+//     // break;
+//   default:
+//     console.log("Invalid day number");
+//     // break;
+// }
+
+// const firstName = "Tushar";
+// const userAge = 18;
+// const isStudent = true;
+
+// const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newArr2 = [5, 4, 3, 2, 1, 20, 10];
+// const newArr3 = [1, 2, 3, 4, 11, 13, 23, 30];
+// newArr.pop();
+// newArr.shift(1,2);
+// const change = newArr.unshift(-2);
+// const change = newArr.concat(newArr2);
+// const change = newArr.slice(2, 5);
+// const change = newArr2.splice(3, 3, 100, 302);
+// const change = newArr2.splice(3, 0, 100, 302);
+// const change = newArr3.sort();
+// const change = newArr2.splice(4, 2);
+// console.log("change", change);
+// console.log(newArr2);
+// console.log(newArr);
+
+// const nameAndNumberList = [["Tushar", 1], ["Ravi", 2], ["Amit", 3]];
+
+// const fruits = ["apple", "banana", "orange", "kiwi"];
+// const newFruits = fruits;
+
+// newFruits.push("mango");
+// newFruits.push("grape");
+// console.log("newFruits", newFruits);
+// console.log("fruits", fruits);
+
+// console.log("hello");
+
+// let num = 0;
+
+// while(num < 10) {
+//     console.log("hello world", num);
+//     num++;
+//     // break;
+// }
+
+// const fruits = ["apple", "banana", "orange", "kiwi"];
+// let i = 0;
+
+// while (fruits.length > i) {
+//   console.log("fruit", fruits[i]);
+//   fruits[i] = fruits[i] + "fruit";
+//   i++;
+// }
+
+// const username = "Shivam";
+// const userAge = 25;
+
+// function sayRamRam() {
+//   console.log(a + b);
+//   var a = 10;
+//   let b = 20;
+// }
+// sayRamRam();
+// console.log(a);
+
+function subtract() {
+  // debugger;
+  const x = 15;
+  const y = 20;
+  const z = 25;
+  console.log(x - y);
+
+  {
+    const num = 77;
+    console.log(num);
+  }
+  function child() {
+    const childName = "Tushar";
+    console.log(childName);
+    console.log(console.log(z));
+  }
+
+  child();
+}
+
+subtract();
